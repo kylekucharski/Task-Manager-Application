d5d0a561958e57ffe6df62724be5ab0dadfe2441
Kyle Kucharski <your.email@example.com>
2024-11-10 17:21:10 -0800

    Edited package.json

--- client/app/reset-password/[resetToken]/page.tsx
+++ client/app/reset-password/[resetToken]/page.tsx
@@ -9,18 +9,18 @@ interface Props {
   };
 }
 
-function page({ params: { resetToken } }: Props) {
+const Page: React.FC<Props> = ({ params: { resetToken } }) => {
   const { resetPassword } = useUserContext();
 
   const [showPassword, setShowPassword] = useState(false);
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
 
-  const handlePasswordChange = (e: any) => {
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
   };
 
-  const handleConfirmPasswordChange = (e: any) => {
+  const handleConfirmPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setConfirmPassword(e.target.value);
   };
 
@@ -28,8 +28,7 @@ function page({ params: { resetToken } }: Props) {
     setShowPassword(!showPassword);
   };
 
-  // handle submit
-  const handleSubmit = (e: any) => {
+  const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
     if (password !== confirmPassword) {
@@ -48,7 +47,7 @@ function page({ params: { resetToken } }: Props) {
             Reset Your Password!
           </h1>
           <div className="relative mt-[1rem] flex flex-col">
-            <label htmlFor="email" className="mb-1 text-[#999]">
+            <label htmlFor="password" className="mb-1 text-[#999]">
               New Password
             </label>
             <input
@@ -73,15 +72,15 @@ function page({ params: { resetToken } }: Props) {
             </button>
           </div>
           <div className="relative mt-[1rem] flex flex-col">
-            <label htmlFor="email" className="mb-1 text-[#999]">
+            <label htmlFor="confirmPassword" className="mb-1 text-[#999]">
               Confirm Password
             </label>
             <input
               type={showPassword ? "text" : "password"}
               value={confirmPassword}
               onChange={handleConfirmPasswordChange}
-              id="password"
-              name="password"
+              id="confirmPassword"
+              name="confirmPassword"
               placeholder="*********"
               className="px-4 py-3 border-[2px] rounded-md outline-[#2ECC71] text-gray-800"
             />
@@ -111,6 +110,6 @@ function page({ params: { resetToken } }: Props) {
       </form>
     </main>
   );
-}
+};
 
-export default page;
+export default Page;
