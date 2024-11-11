b092e7c59d8db0213f405d920bd3502fb54a7b19
Kyle Kucharski <your.email@example.com>
2024-11-10 16:33:31 -0800

    Feat: server deployed

--- client/context/userContext.js
+++ client/context/userContext.js
@@ -9,7 +9,7 @@ const UserContext = React.createContext();
 axios.defaults.withCredentials = true;
 
 export const UserContextProvider = ({ children }) => {
-  const serverUrl = "http://localhost:8000";
+  const serverUrl = "https://task-manager-application-4nbq.onrender.com/api/v1";
 
   const router = useRouter();
 
